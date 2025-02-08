0058f93adb5c4904a3e47d2b71d198ead8cb3d4b
ndri <5224431+ndri@users.noreply.github.com>
2025-02-08 23:20:33 +0100

    Clear build folder in compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+rm -fr .build/*
 qmk json2c layouts/chocofi.json -o keyboards/crkbd/keymaps/custom/keymap.c
 cat turn_off_liatris_power_led.c >> keyboards/crkbd/keymaps/custom/keymap.c
 qmk compile -kb crkbd/rev1/common -km custom -e CONVERT_TO=promicro_rp2040
